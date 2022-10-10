3a07594948834fbd4a32126b1ae06f177fe90a0f
Sirui Lu <siruilu@cs.washington.edu>
2022-10-10 00:12:46 -0700

    progress finished

--- theories/Progress.v
+++ theories/Progress.v
@@ -14,6 +14,18 @@ Open Scope nat.
 
 Create HintDb progress discriminated.
 
+Lemma hm_both_sup_p : forall {T} {P Psub} {n nsub} {ms : MergingStrategy T n}
+  {mssub : MergingStrategy T nsub} {i} {u},
+  SubStrategy Psub P i mssub ms ->
+  HieraricalMergingInv Psub mssub u ->
+  AllInUnion P u.
+Proof.
+  intros.
+  induction u.
+  - invcd H0. constructor. eapply proper_ms_sub_p; eauto.
+  - constructor; eauto with inv. 
+Qed.
+
 Theorem progress : forall {T n t} {ms : MergingStrategy T n},
   EvalTermsGood ms t ->
   (exists u, t ==>* (EvalValue ms u) /\ EvalTermsGood ms (EvalValue ms u)).
@@ -23,8 +35,11 @@ Proof.
   generalize dependent n.
   apply EvalTerms_ind' with (t := t); intros; subst; eauto.
   all: inversion_initial_eval_good; simpl in *; try solve [exfalso; eauto].
+  (* EvalValue *)
   { repeat eexists; eauto with eval. }
+  (* MrgIf concrete *)
   { destruct b; repeat eexists; eauto with eval. }
+  (* MrgIf simple *)
   { repeat eexists; eauto.
     econstructor; econstructor; eauto.
     invcd H7.
@@ -47,9 +62,11 @@ Proof.
       assert (H1:EvalTermsGood ms (hd ms c t f)) by (econstructor; eauto with eval inv union);
       specialize (H H1) as [u [? ?]]; exists u
     end.
+  (* MrgIf dispatch *)
   1-4: specialize_sorted_ms;
     specialize_ih_mrgif_dispatch;
     econstructor; eauto with eval.
+  (* SSLt *)
   { exists (If c t0 f); eauto.
     econstructor; econstructor; eauto with eval.
     assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
@@ -58,6 +75,7 @@ Proof.
     specialize (hm_sub_st H5 H0 H5' et) as [P1 ?].
     eapply HMSortedI; eauto with inv strategy union.
   }
+  (* SSEq *)
   { inversiond H4.
     specialize (hm_sub_st ev H0 H2 et) as [P1 ?].
     specialize (hm_sub_st ev H1 H2 ef) as [P2 ?].
@@ -75,6 +93,7 @@ Proof.
     eapply hm_sup_hm; eauto.
     econstructor; eauto.
   }
+  (* SSGt *)
   { exists (If (pnegsb c) f t0); eauto.
     econstructor; econstructor; eauto with eval.
     assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
@@ -83,6 +102,7 @@ Proof.
     specialize (hm_sub_st H5 H1 H5' ef) as [P1 ?].
     eapply HMSortedI; eauto with inv strategy union.
   }
+  (* SIDeg *)
   { assert (EvalTermsGood (SortedStrategy n ind sub) (SSMrgIf (SortedStrategy n ind sub) c t0 f)).
     { econstructor; eauto with eval union. }
     specialize (H1 _ _ H3) as [u [H3' H3'']].
@@ -90,6 +110,7 @@ Proof.
     intuition.
     econstructor; econstructor; eauto.
   }
+  (* SILt *)
   { assert (EvalTermsGood (SortedStrategy n ind sub) (EvalValue (SortedStrategy n ind sub) (If c t0 (If fc ft ff)))).
     { econstructor. eapply hm_if_no_deg; simpl; eauto with inv.
       eapply (all_in_union_left_most' H1). }
@@ -99,6 +120,7 @@ Proof.
     eapply SILt; eauto.
     econstructor; eauto.
   }
+  (* SIEq *)
   { assert (SubUnion ft (If fc ft ff)) by eauto with union.
     assert (ef0:HieraricalMergingInv P (SortedStrategy n ind sub) ft) by eauto with inv.
     inversiond ef; repeat aiu_simplify.
@@ -132,6 +154,7 @@ Proof.
       specialize (hm_sup_all_in_union_ind H5 H15); repeat aiu_simplify.
       auto.
   }
+  (* SIGt *)
   { assert (SubUnion ft (If fc ft ff)) by eauto with union.
     inversiond ef; repeat aiu_simplify.
     - specialize (all_in_union_left_most' H9); simpl; intro.
@@ -154,7 +177,187 @@ Proof.
 
       constructor; solve_aiu.
   }
-Admitted.
+  (* ISDeg *)
+  { assert (EvalTermsGood (SortedStrategy n ind sub) (SSMrgIf (SortedStrategy n ind sub) c t0 f)).
+    { econstructor; eauto with eval union. }
+    specialize (H1 _ _ H3) as [u [H3' H3'']].
+    exists u.
+    intuition.
+    econstructor; econstructor; eauto.
+  }
+  (* ISLt *)
+  { assert (EvalTermsGood (SortedStrategy n ind sub) (MrgIf (SortedStrategy n ind sub) c tf f)).
+    { econstructor; eauto with eval union inv. }
+    specialize (H5 _ _ H7) as [f' [H5 H5']].
+    invcd H5'.
+    assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
+    specialize (all_in_union_left_most' H0); simpl; intros.
+    specialize (proper_ms_ind_some_is_sub_some H8 H9) as [n' [ev' [s' ?]]].
+    specialize (proper_ms_sub_from_subfunc H8 H9 H12) as [Psub ?].
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) tt) by eauto with inv.
+    assert (HieraricalMergingInv Psub s' tt) by eauto with subinv.
+    exists (If (pandsb c tc) tt f').
+    intuition.
+    - econstructor. eapply ISLt; eauto. econstructor.
+    - econstructor. eapply HMSortedI. 4: apply H12.
+      all: repeat aiu_simplify; eauto. all: eauto 4 with inv.
+      eapply eval_do_not_change_index_lowerbound; eauto.
+      simpl. intuition; eauto 3 with union.
+      invcd et; try solve_aiu.
+      repeat aiu_simplify. assert (z = tfi) by eauto with union. lia.
+  }
+  (* ISEq *)
+  { assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
+    specialize (all_in_union_left_most' H1); simpl; intros.
+    specialize (proper_ms_sub_from_subfunc H7 H8 H4) as [Psub ?].
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) tt) by eauto with inv.
+    assert (HieraricalMergingInv Psub s tt) by eauto with subinv.
+    assert (EvalTermsGood s (MrgIf s c tt f)).
+    { econstructor; eauto with subinv. }
+    specialize (H5 _ _ H13) as [t' [H5 H5']].
+    invcd H5'.
+    assert (HieraricalMergingInv Psub s t') by eauto with inv.
+    exists (If (porsb (pnegsb c) tc) t' tf).
+    intuition.
+    - econstructor. eapply ISEq; eauto. econstructor.
+    - econstructor. eapply HMSortedI. 4: apply H4.
+      all: repeat aiu_simplify; eauto. all: eauto 3 with inv.
+      + eapply hm_both_sup_p; eauto.
+      + eapply eval_do_not_change_index_sub_eq; simpl; eauto; intuition; eauto.
+      + invcd et; try solve_aiu.
+        repeat aiu_simplify. assert (z = tfi) by eauto 4 with union. lia.
+  }
+  (* ISGt *)
+  { assert (EvalTermsGood (SortedStrategy n ind sub)
+                          (EvalValue (SortedStrategy n ind sub) (If (pnegsb c) f (If tc tt tf)))).
+    { econstructor. eapply hm_if_no_deg; simpl in *; eauto.
+      exact (all_in_union_left_most' H0).
+      lia.
+    }
+    specialize (H5 _ _ H7) as [u [H5 H5']]. 
+    exists u.
+    invcd_eval_value.
+    intuition.
+    econstructor. eapply ISGt; eauto. constructor.
+  }
+
+  (* IIDeg1 *)
+  { assert (AllInUnion (fun x => AllInUnion (fun y => ind x = ind y) t0) t0) by eauto with union.
+    assert (EvalTermsGood (SortedStrategy n ind sub) (SIMrgIf (SortedStrategy n ind sub) c t0 f)).
+    { eauto with eval. }
+    specialize (H1 _ _ H4) as [u [H1 H1']].
+    exists u.
+    intuition.
+    econstructor. eapply IIDeg1; eauto. constructor.
+  }
+
+  (* IIDeg2 *)
+  { assert (AllInUnion (fun x => AllInUnion (fun y => ind x = ind y) f) f) by eauto with union.
+    assert (EvalTermsGood (SortedStrategy n ind sub) (ISMrgIf (SortedStrategy n ind sub) c (If tc tt tf) f)).
+    { eauto with eval. }
+    specialize (H4 _ _ H7) as [u [H4 H4']].
+    exists u.
+    intuition.
+    econstructor. eapply IIDeg2; eauto. constructor.
+  }
+
+  (* IILt *)
+  { assert (EvalTermsGood (SortedStrategy n ind sub)
+                          (MrgIf (SortedStrategy n ind sub) c tf (If fc ft ff))).
+    { econstructor; eauto with eval union inv. }
+    specialize (H7 _ _ H9) as [f' [H7 H7']].
+    exists (If (pandsb c tc) tt f').
+
+    intuition.
+    econstructor. eapply IILt; eauto. constructor.
+
+    assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
+    specialize (all_in_union_left_most' H0); simpl; intros.
+    specialize (proper_ms_ind_some_is_sub_some H10 H11) as [n' [ev' [s' ?]]].
+    specialize (proper_ms_sub_from_subfunc H10 H11 H12) as [Psub ?].
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) tt) by eauto 3 with inv.
+    assert (HieraricalMergingInv Psub s' tt) by eauto with subinv.
+    invcd H7'.
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) f') by eauto 3 with inv.
+
+    econstructor. eapply HMSortedI; eauto; repeat aiu_simplify; eauto 3 with inv.
+    eapply eval_do_not_change_index_lowerbound; eauto.
+    simpl. intuition; eauto 3 with union.
+    invcd et; try solve_aiu.
+    repeat aiu_simplify. assert (z = tfi) by eauto with union. lia.
+
+    eapply hm_lm_is_lowerbound; eauto.
+    simpl. exact (all_in_union_left_most' H2).
+  }
+
+  (* IIEq *)
+  { assert (EvalTermsGood (SortedStrategy n ind sub)
+                          (MrgIf (SortedStrategy n ind sub) c tf ff)).
+    { econstructor; eauto with eval union inv. }
+    specialize (H8 _ _ H10) as [f' [H8 H8']].
+
+    assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
+    specialize (all_in_union_left_most' H0); simpl; intros.
+    specialize (proper_ms_sub_from_subfunc H11 H12 H6) as [Psub ?].
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) tt) by eauto 2 with inv.
+    assert (HieraricalMergingInv Psub s tt) by eauto with subinv.
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) ft) by eauto 2 with inv.
+    assert (HieraricalMergingInv Psub s ft) by eauto with subinv.
+    assert (EvalTermsGood s (MrgIf s c tt ft)).
+    { econstructor; eauto with subinv. }
+    specialize (H7 _ _ H21) as [t' [H7 H7']].
+    exists (If (pitesb c tc fc) t' f').
+
+    intuition.
+    econstructor. eapply IIEq; eauto. constructor.
+
+    invcd H8'.
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) f') by eauto 3 with inv.
+    invcd H7'.
+    assert (HieraricalMergingInv Psub s t') by eauto 3 with inv.
+
+    econstructor. eapply HMSortedI; eauto; repeat aiu_simplify; eauto 3 with inv.
+
+    - eapply hm_both_sup_p; eauto.
+    - eapply eval_do_not_change_index_sub_eq; simpl; eauto; intuition; eauto.
+    - eapply eval_do_not_change_index_lowerbound; eauto.
+      simpl. intuition; eauto 3 with union.
+      + invcd et; try solve_aiu.
+        repeat aiu_simplify. assert (z = tfi) by eauto 4 with union. lia.
+      + invcd ef; try solve_aiu.
+        repeat aiu_simplify. assert (z = ffi) by eauto 4 with union. lia.
+  }
+
+  (* IIGt *)
+  { assert (EvalTermsGood (SortedStrategy n ind sub)
+                          (MrgIf (SortedStrategy n ind sub) c (If tc tt tf) ff)).
+    { econstructor; eauto with eval union inv. }
+    specialize (H7 _ _ H9) as [f' [H7 H7']].
+    exists (If (pandsb (pnegsb c) fc) ft f').
+
+    assert (ProperStrategy P (SortedStrategy n ind sub)) by eauto with inv.
+    specialize (all_in_union_left_most' H2); simpl; intros.
+    specialize (proper_ms_ind_some_is_sub_some H10 H11) as [n' [ev' [s' ?]]].
+    specialize (proper_ms_sub_from_subfunc H10 H11 H12) as [Psub ?].
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) ft) by eauto 3 with inv.
+    assert (HieraricalMergingInv Psub s' ft) by eauto with subinv.
+    invcd H7'.
+    assert (HieraricalMergingInv P (SortedStrategy n ind sub) f') by eauto 3 with inv.
+
+    intuition.
+    econstructor. eapply IIGt; eauto. constructor.
+
+    econstructor. eapply HMSortedI; eauto; repeat aiu_simplify; eauto 3 with inv.
+    eapply eval_do_not_change_index_lowerbound; eauto.
+    simpl. intuition; eauto 3 with union.
+
+    - assert (fti < tti)%Z by lia. eapply hm_lm_is_lowerbound; eauto.
+      simpl. exact (all_in_union_left_most' H0). 
+
+    - invcd ef; try solve_aiu.
+      repeat aiu_simplify. assert (z = ffi) by eauto with union. lia.
+  }
+Qed.
 
 
   
